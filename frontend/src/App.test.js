5bca63133c048c53e99e126ff59331f783cdf5fb
zouhair <zouhairmohamar@gmail.com>
2025-10-02 16:53:15 -0400

    Update App.test.js to match actual app content

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders navigation links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const homeLink = screen.getByText(/home/i);
+  const greetingLink = screen.getByText(/greeting/i);
+  const aboutLink = screen.getByText(/about/i);
+  expect(homeLink).toBeInTheDocument();
+  expect(greetingLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
 });
